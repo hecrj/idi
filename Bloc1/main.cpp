57661aae6cdfb412320f28e3a9a89006e633725d
Hector Ramon Jimenez <hector.ramon@b5s202pc52.fib.upc.es>
2013-09-10 08:22:25 +0200

    Initial commit.

---
+++ Bloc1/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: hector.ramon
+ *
+ * Created on September 10, 2013, 8:09 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
