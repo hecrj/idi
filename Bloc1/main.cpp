435c8f1cc73b030730e3c9f988807a15df4510d4
Héctor <hector0193@gmail.com>
2013-09-17 10:04:52 +0200

    Library started.

--- Bloc1/main.cpp
+++ Bloc1/main.cpp
@@ -4,331 +4,18 @@
  *
  * Created on September 10, 2013, 8:09 AM
  */
-#if defined(__APPLE__)
-  #include <OpenGL/OpenGl.h>
-  #include <GLUT/GLUT.h>
-#else
-  #include <GL/gl.h>
-  #include <GL/freeglut.h>
-#endif
 
-#include <iostream>
-
-using namespace std;
-
-struct Vertex
-{
-    double x, y, z;
-    
-    Vertex(double x, double y, double z)
-    {
-        this->x = x;
-        this->y = y;
-        this->z = z;
-    }
-    
-    ~Vertex()
-    { }
-    
-    void draw()
-    {
-        glVertex3d(x, y, z);
-    }
-};
-
-class Triangle
-{
-    Vertex* a;
-    Vertex* b;
-    Vertex* c;
-    
-public:
-    static Triangle* def()
-    {
-        return new Triangle(
-            new Vertex(-0.5, -1.0 / 3.0, 0),
-            new Vertex(0.5, -1.0 / 3.0, 0),
-            new Vertex(0, 2.0 / 3.0, 0)
-        );
-    }
-    
-    Triangle(Vertex* a, Vertex* b, Vertex *c)
-    {
-        this->a = a;
-        this->b = b;
-        this->c = c;
-    }
-    
-    ~Triangle()
-    {
-        delete a;
-        delete b;
-        delete c;
-    }
-    
-    void draw()
-    {
-        glBegin(GL_TRIANGLES);
-        glColor3d(1, 0, 0);
-        
-        a->draw();
-        b->draw();
-        c->draw();
-        
-        glEnd();
-    }
-};
-
-struct Viewport
-{
-    int x;
-    int y;
-    int width;
-    int height;
-    
-    Viewport(int width, int height)
-    {
-        x = 0;
-        y = 0;
-        this->width = width;
-        this->height = height;
-    }
-    
-    void reshape(int windowWidth, int windowHeight)
-    {
-        x = y = 0;
-        width = windowWidth;
-        height = windowHeight;
-        
-        if(width > height)
-        {
-            x = (width - height) / 2;
-            width = height;
-        }
-        else
-        {
-            y = (height - width) / 2;
-            height = width;
-        }
-
-        glViewport(x, y, width, height);
-    }
-    
-    Vertex* getVertex(int mx, int my)
-    {
-        mx = mx - x;
-        my = (height - my) + y;
-        
-        double vx = ((double)mx / width) * 2 - 1;
-        double vy = ((double)my / height) * 2 - 1;
-        
-        return new Vertex(vx, vy, 0);
-    }
-};
-
-struct Window
-{
-    int width;
-    int height;
-    Viewport* viewport;
-    
-    Window(int width, int height)
-    {
-        this->width = width;
-        this->height = height;
-        
-        viewport = new Viewport(width, height);
-    }
-    
-    void reshape(int width, int height)
-    {
-        this->width = width;
-        this->height = height;
-        
-        viewport->reshape(width, height);
-    }
-    
-    void init(int* argc, char** argv)
-    {
-        glutInit(argc, argv);
-        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-        glutInitWindowSize(width, height);
-        glutCreateWindow("Laboratori IDI - Bloc 1");
-    }
-    
-    Vertex* getViewportVertex(int x, int y)
-    {
-        return viewport->getVertex(x, y);
-    }
-};
-
-class State
-{
-    unsigned char keyTrigger;
-    
-public:
-    State(unsigned char keyTrigger)
-    {
-        this->keyTrigger = keyTrigger;
-    }
-    
-    char getKeyTrigger()
-    {
-        return keyTrigger;
-    }
-    
-    // Description
-    virtual string getDescription(){ return "Unknow description"; }
-    
-    // Actions called when entering/leaving a state
-    virtual void enter()
-    {
-        cout << "-------------------" << endl;
-        cout << getDescription() << endl;
-    }
-    
-    virtual void leave() { }
-    
-    // Default mouse state actions
-    virtual void mouseMotion(int x, int y){ }
-    virtual void mousePressed(int buttonId, int released, int x, int y) { }
-};
-
-class BackgroundSelector : public State
-{
-    Window* window;
-    
-public:
-    BackgroundSelector(unsigned char keyTrigger, Window* window)
-            : State(keyTrigger)
-    {
-        this->window = window;
-    }
-    
-    string getDescription()
-    {
-        return "Background selector:\n"
-                "Hold the mouse pressed and move it around the window to select the background color.";
-    }
-    
-    void mouseMotion(int x, int y)
-    {
-        double red = getRangedValue(x, window->width);
-        double blue = getRangedValue(y, window->height);
-        
-        glClearColor(red, 0, blue, 1);
-        glutPostRedisplay();
-    }
-    
-private:
-    double getRangedValue(int k, int max)
-    {
-        if(k < 0)
-            k = -k;
-        
-        int a = k / max;
-        
-        double ranged = (double)(k % max);;
-        
-        if(a % 2 != 0)
-            ranged = max - ranged;
-            
-        return ranged / max;
-    }
-};
-
-class TriangleSelector : public State
-{
-    Window* window;
-    
-    Triangle** triangle;
-    Vertex* a;
-    Vertex* b;
-    Vertex* c;
-    bool completed;
-    
-public:
-    TriangleSelector(unsigned char keyTrigger, Window* window, Triangle** triangle)
-            : State(keyTrigger)
-    {
-        this->window = window;
-        this->triangle = triangle;
-        
-        a = b = c = NULL;
-    }
-    
-    string getDescription()
-    {
-        return "Triangle selector:\n"
-                "Click to define every vertex of the new triangle.";
-    }
-    
-    void enter()
-    {
-        a = b = c = NULL;
-        
-        State::enter();
-    }
-    
-    void leave()
-    {
-        if(a != NULL)
-            delete a;
-        
-        if(b != NULL)
-            delete b;
-        
-        if(c != NULL)
-            delete c;
-    }
-    
-    void mousePressed(int buttonId, int released, int x, int y)
-    {
-        if(released == 0)
-            return;
-        
-        Vertex* v = window->getViewportVertex(x, y);
-        
-        cout << "New vertex defined at: (" << v->x << ", " << v->y << ", " << v->z << ")" << endl;
-        
-        if(a == NULL)
-            a = v;
-        else if(b == NULL)
-            b = v;
-        else
-            c = v;
-        
-        if(c != NULL)
-        {
-            *triangle = new Triangle(a, b, c);
-            a = b = c = NULL;
-            
-            glutPostRedisplay();
-        }
-    }
-};
-
-#define WINDOW_WIDTH_DEFAULT  600
-#define WINDOW_HEIGHT_DEFAULT 600
-#define STATE_NUM             2
+// Load generic code
+#include <idi.cpp>
 
 // Global variables
 Triangle* triangle;
 Window* window;
-State* availableStates[STATE_NUM];
-State* currentState;
+StateMachine* states;
 
 void help()
 {
-    cout << "-------------------" << endl;
-    cout << "Available actions:" << endl;
-
-    for(int i = 0; i < STATE_NUM; ++i)
-    {
-        cout << availableStates[i]->getDescription() << endl;
-        cout << "Press '" << availableStates[i]->getKeyTrigger() << "' to enable it." << endl;
-        cout << endl;
-    }
+    states->printHelp();
     
     cout << "Press 'h' to show this help message." << endl;
     cout << "Press ESC to quit." << endl;
@@ -350,12 +37,12 @@ void reshape(int width, int height)
 
 void mousePressed(int buttonId, int released, int x, int y)
 {
-    currentState->mousePressed(buttonId, released, x, y);
+    states->getCurrentState()->mousePressed(buttonId, released, x, y);
 }
 
 void mouseMotion(int x, int y)
 {    
-    currentState->mouseMotion(x, y);
+    states->getCurrentState()->mouseMotion(x, y);
 }
 
 void keyPressed(unsigned char key, int x, int y)
@@ -369,25 +56,17 @@ void keyPressed(unsigned char key, int x, int y)
         return;
     }
     
-    for(int i = 0; i < STATE_NUM; ++i)
-    {
-        if(availableStates[i]->getKeyTrigger() == key)
-        {
-            currentState->leave();
-            currentState = availableStates[i];
-            currentState->enter();
-        }
-    }
+    states->trigger(key);
 }
 
 void initStates()
 {
-    availableStates[0] = new BackgroundSelector('f', window);
-    availableStates[1] = new TriangleSelector('t', window, &triangle);
+    states = new StateMachine();
     
-    // Default state
-    currentState = availableStates[0];
-    currentState->enter();
+    states->add('f', new BackgroundSelector(window));
+    states->add('t', new TriangleSelector(window, &triangle));
+    
+    states->init();
 }
 
 void initGL(int argc, char **argv)
@@ -396,7 +75,7 @@ void initGL(int argc, char **argv)
     triangle = Triangle::def();
     
     // Default window
-    window = new Window(WINDOW_WIDTH_DEFAULT, WINDOW_HEIGHT_DEFAULT);
+    window = new Window();
     
     // Init states
     initStates();
---
+++ Bloc2/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: hector0193
+ *
+ * Created on 17 de septiembre de 2013, 8:39
+ */
+
+#include <idi.cpp>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
